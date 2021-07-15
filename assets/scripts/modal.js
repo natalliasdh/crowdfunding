08e13c6b72bcc8c2d7948d6c46ed13abf65a829f
natalliasdh <natallia.sdh@gmail.com>
2021-07-15 15:07:29 -0400

    Modal mobile

---
+++ assets/scripts/modal.js
@@ -0,0 +1,9 @@
+const backProject = document.querySelector("#back");
+const modal = document.querySelector("#modal");
+
+backProject.addEventListener('click', () => {
+
+    modal.style.display = 'block';
+
+
+});
