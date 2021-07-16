bcd1fe85dbeba612d4d5cb79244ea43c4fc23ddf
natalliasdh <natallia.sdh@gmail.com>
2021-07-15 20:12:29 -0400

    Design and modals ready

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -1,9 +1,42 @@
 const backProject = document.querySelector("#back");
 const modal = document.querySelector("#modal");
+const thanks = document.querySelector(".thanks");
+const close = document.querySelector(".close");
+const editionButton = document.querySelector("#editionbutton");
+const bambooButton = document.querySelector("#bamboobutton");
+const gotIt = document.querySelector("#gotit");
 
 backProject.addEventListener('click', () => {
 
     modal.style.display = 'block';
+});
+
+close.addEventListener('click', () => {
+
+    modal.style.display = 'none';
+});
 
 
+gotIt.addEventListener('click', () => {
+
+    thanks.style.display = 'none';
+});
+
+window.addEventListener('click', (e) => {
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    }
+});
+
+
+bambooButton.addEventListener('click', () => {
+
+    modal.style.display = 'none';
+    thanks.style.display = 'block';
+});
+
+editionButton.addEventListener('click', () => {
+
+    modal.style.display = 'none';
+    thanks.style.display = 'block';
 });
