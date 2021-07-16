2c6f88b13d1b2252eec2d7efdd82384abe12a978
natalliasdh <natallia.sdh@gmail.com>
2021-07-15 20:45:51 -0400

    Radionbuttons functionality

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -5,10 +5,32 @@ const close = document.querySelector(".close");
 const editionButton = document.querySelector("#editionbutton");
 const bambooButton = document.querySelector("#bamboobutton");
 const gotIt = document.querySelector("#gotit");
+const content = document.querySelector(".content");
+const invisible = document.querySelector(".invisible");
+const invisible2 = document.querySelector(".invisible2");
+const bambooRadio = document.querySelector("#bambooreward");
+const editionRadio = document.querySelector("#editionreward");
 
-backProject.addEventListener('click', () => {
+content.addEventListener('click', (e) => {
+    if ((e.target.tagName === 'BUTTON') && (e.target.className !== 'bookmark')) {
+        modal.style.display = 'block';
+    }
 
-    modal.style.display = 'block';
+    if (e.target.id == "bamrew") {
+        invisible2.style.display = "none";
+        invisible.style.display = "block";
+        bambooRadio.checked = true;
+
+    }
+
+
+    if (e.target.id == "edrew") {
+        invisible2.style.display = "block";
+        invisible.style.display = "none";
+        editionRadio.checked = true;
+    }
+
+
 });
 
 close.addEventListener('click', () => {
@@ -39,4 +61,25 @@ editionButton.addEventListener('click', () => {
 
     modal.style.display = 'none';
     thanks.style.display = 'block';
+});
+
+
+modal.addEventListener("click", (e) => {
+
+    if (e.target.id == "bambooreward") {
+        invisible.style.display = "block";
+        invisible2.style.display = "none";
+    }
+
+
+    if (e.target.id == "editionreward") {
+        invisible2.style.display = "block";
+        invisible.style.display = "none";
+    }
+
+    if (e.target.id == "noreward") {
+        invisible2.style.display = "none";
+        invisible.style.display = "none";
+    }
+
 });
